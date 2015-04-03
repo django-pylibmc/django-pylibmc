097f1b4c6b86cbd1fb7bbfa461a90aec997999a5
John Whitlock <John-Whitlock@ieee.org>
2015-04-02 22:28:14 -0500

    Release 0.6.0

---
+++ CHANGELOG.rst
@@ -0,0 +1,10 @@
+Changelog
+=========
+
+0.6.0 - 2015-04-01
+------------------
+- Requires pylibmc 1.4.1 or greater
+- Supports Django 1.4 through 1.8.
+- Supports Python 2.5 through 2.7, and Python 3.3 through 3.4
+- In Django 1.6 and higher, when the timeout is omitted, the default
+  timeout is used, rather than set to "never expire".
--- setup.py
+++ setup.py
@@ -18,11 +18,14 @@ class Tox(TestCommand):
         sys.exit(errno)
 
 
+long_description = "\n".join(
+    [open(f).read() for f in ['README.rst', 'CHANGELOG.rst']])
+
 setup(
     name='django-pylibmc',
     version=django_pylibmc.__version__,
     description='Django cache backend using pylibmc',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     author='Jeff Balogh',
     author_email='jbalogh@mozilla.com',
     url='https://github.com/django-pylibmc/django-pylibmc',
@@ -33,6 +36,7 @@ setup(
     install_requires=['pylibmc>=1.4.1', 'Django>=1.4'],
     tests_require=['tox'],
     cmdclass = {'test': Tox},
+    keywords = 'django cache pylibmc memcached',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
@@ -43,6 +47,13 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
